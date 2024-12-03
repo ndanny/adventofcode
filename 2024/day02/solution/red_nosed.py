7e1b7d379bec5d85eb043336e41bbf957a2e7f0d
Danny Nguyen <ndanny@github.com>
2024-12-02 17:04:36 -0800

    Attempt day 02 part 2

--- 2024/day02/solution/red_nosed.py
+++ 2024/day02/solution/red_nosed.py
@@ -54,6 +54,75 @@ Part 2
 """
 safe_reports = 0
 
+def next_greater(levels: list[int]):
+    # Calculate x-indices away until we see the next inc number.
+    n = len(levels)
+    stack = []
+    result = [-1] * n
+    for i in range(n):
+        while stack and levels[i] > levels[stack[-1]]:
+            idx = stack.pop()
+            result[idx] = i - idx
+        stack.append(i)
+    return result
+
+def next_smaller(levels: list[int]):
+    # Calculate x-indices away until we see the next dec number.
+    n = len(levels)
+    stack = []
+    result = [-1] * n
+    for i in range(n):
+        while stack and levels[i] < levels[stack[-1]]:
+            idx = stack.pop()
+            result[idx] = i - idx
+        stack.append(i)
+    return result
+
+def is_valid(levels: list[int], monotonic: list[int]) -> bool:
+    """Given the levels input and the monotonic result, determine if the report
+    meets the criteria for being safe with at most one penalty.
+    """
+    # We can only afford 1 penalty at most.
+    # Track the bad index.
+    penalty, bad_idx = 0, None
+
+    # Calculate the count of non-1s.
+    # if monotonic[i] != 1, then the next increasing or decreasing value is not
+    # 1-away and we can penalize.
+    for i in range(len(monotonic) - 1):
+        if monotonic[i] != 1:
+            penalty += 1
+            bad_idx = i
+            if penalty > 1:
+                return False
+
+    levels_copy = levels[:]
+    if bad_idx is not None:
+        del levels_copy[bad_idx]
+
+    # Sanity check the range of each adjacent diff.
+    for i in range(1, len(levels_copy)):
+        adjacent_diff = abs(levels_copy[i] - levels_copy[i - 1])
+        if adjacent_diff not in range(0, 4):
+            penalty += 1
+            if penalty > 1:
+                return False
+
+    return True
+
+def check_report_v2(levels: list[int]) -> bool:
+    # Calculate x-indices way until we see the next inc/dec number.
+    inc = next_greater(levels)
+    dec = next_smaller(levels)
+    inc_valid = is_valid(levels, inc)
+    dec_valid = is_valid(levels, dec)
+
+    # print("for", levels, ":")
+    # print("\tIncreasing:", inc, inc_valid)
+    # print("\tDecreasing:", dec, dec_valid)
+
+    return inc_valid or dec_valid
+
 with open(input_path) as file:
     for report in file:
         levels = list(map(int, report.split(' ')))
@@ -62,6 +131,7 @@ with open(input_path) as file:
             safe_reports += 1
             continue
 
-        # TODO: implement the code here.
+        if check_report_v2(levels):
+            safe_reports += 1
 
 print(f'Safe reports with at most one removal: {safe_reports}')
