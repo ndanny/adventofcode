2176861bc90174f12323660584c108e5ab8b030d
Danny Nguyen <ndanny@github.com>
2024-12-02 20:43:55 -0800

    Solve 2023 day 2 part 2

--- 2023/day02/solution/cube.py
+++ 2023/day02/solution/cube.py
@@ -1,8 +1,8 @@
 """
 Part 1
 
-- Detect the first and last digit in the string, concatenate them,
-convert to an int, and add to a resulting counter.
+- Parse input, and for each subset, check if the color count surpasses the
+max thresholds defined for each color.
 """
 from pathlib import Path
 
@@ -37,4 +37,32 @@ with open(input_path) as file:
         if is_possible(data):
             result += game_id
 
-print(result)
+print('Part 1:', result)
+
+"""
+Part 2
+
+- Similar to above, with some math and statefulness.
+"""
+result = 0
+
+def get_cube_power(data: str) -> int:
+    minimums = {'red': 0, 'green': 0, 'blue': 0}
+    subsets = data.split(';')
+    for subset in subsets:
+        subset = subset.lstrip().rstrip('\n')
+        dice = subset.split(', ')
+        for pair in dice:
+            count, color = pair.split(' ')
+            count = int(count)
+            minimums[color] = max(minimums[color], count)
+    return minimums['red'] * minimums['green'] * minimums['blue']
+
+with open(input_path) as file:
+    for line in file:
+        game, data = line.split(':')
+        game_id = int(game.split(' ')[1])
+        power = get_cube_power(data)
+        result += power
+
+print('Part 2:', result)
