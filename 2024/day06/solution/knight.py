770c479317ebe0164483b6f1e97e9ac17362beee
Danny Nguyen <ndanny@github.com>
2024-12-06 00:27:54 -0800

    Solve 2024 day 06 part 2

--- 2024/day06/solution/knight.py
+++ 2024/day06/solution/knight.py
@@ -10,6 +10,7 @@ Part 1
 
 Ans: 4656
 """
+import copy
 from pathlib import Path
 
 input_path = Path(__file__).parent / '../input/input.txt'
@@ -32,28 +33,29 @@ for i in range(len(matrix)):
 
 print(f"Running simulation on a {len(matrix)}x{len(matrix[0])} matrix starting at {start}.")
 
+mapping = {
+    '^': (-1, 0),
+    '>': (0, 1),
+    'v': (1, 0),
+    '<': (0, -1),
+}
+
+next_pos = {
+    '^': '>',
+    '>': 'v',
+    'v': '<',
+    '<': '^',
+}
+
 def print_matrix(matrix: list[list[str]]):
     for row in matrix:
         print(row)
     print()
 
 def run_simulation(matrix: list[list[str]], start: tuple) -> int:
+    """Returns a count of the unique cells visited by the knight."""
     visited = {start}
     r, c = start
-
-    mapping = {
-        '^': (-1, 0),
-        '>': (0, 1),
-        'v': (1, 0),
-        '<': (0, -1),
-    }
-
-    next_pos = {
-        '^': '>',
-        '>': 'v',
-        'v': '<',
-        '<': '^',
-    }
 
     def escaped(row: int, col: int):
         return row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[0])
@@ -75,17 +77,63 @@ def run_simulation(matrix: list[list[str]], start: tuple) -> int:
         visited.add((r, c))
         # print_matrix(matrix)
 
-result = run_simulation(matrix, start)
+result = run_simulation(copy.deepcopy(matrix), start)
 
 print('Part 1:', result)
 
 """
 Part 2
 
-- <Add notes here>
+- In any of the matrix[i][j] == '.' cells, replace it with any char (like O) and
+determine if there is an infinite loop.
+- Count the possible infinite loops by placing obstacles in '.' spots.
 
-Ans: XXX
+Ans: 1575
 """
 
+def run_simulation_with_alchemical_retroencabulator(matrix: list[list[str]], start: tuple) -> bool:
+    """Returns True if the knight can escape. Returns False if there is an infinite loop
+    caused by the alchemical retroencabulator placement."""
+    r, c = start
+    visited = {
+        (r, c): 1
+    }
+
+    def escaped(row: int, col: int):
+        return row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[0])
+
+    while True:
+        next_r = r + mapping[matrix[r][c]][0]
+        next_c = c + mapping[matrix[r][c]][1]
+
+        if escaped(next_r, next_c):
+            return True
+        elif matrix[next_r][next_c] == '#' or matrix[next_r][next_c] == 'O':
+            matrix[r][c] = next_pos[matrix[r][c]]
+        elif matrix[next_r][next_c] == '.':
+            icon = matrix[r][c]
+            matrix[r][c] = '.'
+            matrix[next_r][next_c] = icon
+            r, c = next_r, next_c
+
+        if (r, c) not in visited:
+            visited[(r, c)] = 1
+        else:
+            visited[(r, c)] += 1
+
+        if visited[(r, c)] >= 100:
+            return False
+
 result = 0
+
+# WARNING! This will take awhile. It ran fo 1m 27s on my Macbook Pro M3.
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        if matrix[i][j] == '.':
+            print(f'Attempting simulation with retroencabulator placed at {i},{j}')
+            matrix_copy = copy.deepcopy(matrix)
+            matrix_copy[i][j] = 'O' # alchemical retroencabulator
+            if run_simulation_with_alchemical_retroencabulator(matrix_copy, start) == False:
+                result += 1
+
 print('Part 2:', result)
