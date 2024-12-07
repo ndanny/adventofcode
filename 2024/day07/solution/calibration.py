15dcb3a7212d04bb7a1cb8f5b37eabee36342537
Danny Nguyen <ndanny@github.com>
2024-12-06 22:53:57 -0800

    Setup 2024 day 07

---
+++ 2024/day07/solution/calibration.py
@@ -0,0 +1,29 @@
+"""
+Part 1
+
+- <Add notes here>
+
+Ans: XXX
+"""
+from pathlib import Path
+
+input_path = Path(__file__).parent / '../input/input.txt'
+
+result = 0
+
+with open(input_path) as file:
+    for line in file:
+        pass
+
+print('Part 1:', result)
+
+"""
+Part 2
+
+- <Add notes here>
+
+Ans: XXX
+"""
+
+result = 0
+print('Part 2:', result)
