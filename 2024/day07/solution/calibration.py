e3163987565f9cb02f388c92cdac41d41b4899eb
Danny Nguyen <ndanny@github.com>
2024-12-06 23:49:49 -0800

    Solve 2024 day 07 part 2

--- 2024/day07/solution/calibration.py
+++ 2024/day07/solution/calibration.py
@@ -39,10 +39,35 @@ print('Part 1:', result)
 """
 Part 2
 
-- <Add notes here>
-
-Ans: XXX
+Ans: 275791737999003
 """
 
 result = 0
+
+def is_valid_v2(test_value: int, nums: list[int]) -> bool:
+    combos = [nums[0]]
+
+    for i in range(1, len(nums)):
+        temp = []
+        for c in combos:
+            temp.append(nums[i] * c)
+            temp.append(nums[i] + c)
+            temp.append(int(str(c) + str(nums[i])))
+        combos = temp
+
+    for num in combos:
+        if num == test_value:
+            return True
+
+    return False
+
+with open(input_path) as file:
+    for line in file:
+        test_value, nums = line.split(": ")
+        test_value = int(test_value)
+        nums = [int(n) for n in nums.split(" ")]
+
+        if is_valid_v2(test_value, nums):
+            result += test_value
+
 print('Part 2:', result)
